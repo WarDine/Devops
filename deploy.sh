aa2842b47a14b27244f70e94179ccd5a742999b8
Trăistaru Andrei-Cristian <andrei.traistaru99@stud.acs.upb.ro>
2022-04-20 17:32:04 +0300

    Portainer service added. #5

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+sudo docker swarm init
 sudo docker build ./ui -t ui
 sudo docker build ./recipe-manager-service -t recipe-manager-service
 sudo docker build ./mass-halls-manager-service -t mass-halls-manager-service
 
 sudo docker stack deploy -c docker-compose.yml WarDineStack
+
+sudo docker stack deploy -c portainer.yml WarDineStack
+sudo docker stack ps  WarDine
