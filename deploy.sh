563a64a63c70e53065cd01db1a13b1d2969dfba7
Iulian Olaru <iulianolaru249@yahoo.com>
2022-05-22 16:53:33 +0300

    Change kong configs

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 # sudo docker swarm init
 
-# sudo docker build ./ui -t ui
+docker build ./frontend -t frontend
 docker build ../messhallmanager -t messhallmanager
 docker build ../recipemanager -t recipemanager
-docker build ../mailing -t mailing
+#docker build ../mailing -t mailing
 
 docker-compose up
 # sudo docker stack deploy -c docker-compose-portainer.yml PortainerStack
